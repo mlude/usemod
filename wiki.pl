14c06ba45d83537da862a291a9d0cf0b553bf525
Markus Lude <markus.lude@gmx.de>
2017-12-01 08:25:46 +0100

    some preparations for 1.2.1: bump version

--- wiki.pl
+++ wiki.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl -wT
-# UseModWiki version 1.2.0 (November 05, 2017)
+# UseModWiki version 1.2.1 (December 01, 2017)
 # Copyright (C) 2000-2003 Clifford A. Adams  <caadams@usemod.com>
 # Copyright (C) 2002-2003 Sunir Shah  <sunir@sunir.org>
 # with some changes from  Markus Lude <markus.lude@gmx.de>
@@ -5120,7 +5120,7 @@ sub RenamePage {
 
 sub DoShowVersion {
   print &GetHeader('', T('Displaying Wiki Version'), '');
-  print "<p>UseModWiki version 1.2.0</p>\n";
+  print "<p>UseModWiki version 1.2.1</p>\n";
   print &GetCommonFooter();
 }
 
